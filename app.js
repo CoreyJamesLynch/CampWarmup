aba97cb2686a007a2d81b6718632b40fc510192b
bearinawolfpack <bearinawolfpack@gmail.com>
2021-07-08 14:50:52 -0400

    Config Express server and connect

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
+require('dotenv').config();
 // Express webserver
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 // Express Routes
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(port, function (req, res) {
+  console.log('Listening on port ' + port);
+});
+
 // Mongoose connection to MongoDB
