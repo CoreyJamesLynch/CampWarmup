be0264b249504e7a2cfd60d6e23976bcb1cd5f19
bearinawolfpack <bearinawolfpack@gmail.com>
2021-07-08 15:33:03 -0400

    Create campground Schema and model

--- models/campground.js
+++ models/campground.js
@@ -1,2 +1,10 @@
-// Describes a campground
-  // a campground has a title, price, description, and location
+const campgroundSchema = new mongoose.Schema({
+  title: String,
+  price: Number,
+  description: String,
+  location: String,
+});
+
+const Campground = mongoose.model('Campground', campgroundSchema);
+
+module.exports = Campground;
