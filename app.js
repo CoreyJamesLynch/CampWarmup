49671a6f2cbce249be771d73d4894197a0da012c
bearinawolfpack <bearinawolfpack@gmail.com>
2021-07-19 12:29:07 -0400

    Change and style edit view to reflect new campground model
    Add inputs to reflect additional campground data fields
    
    Add Bootstrap styles to edit view
    
    Update edit route in app.js

--- app.js
+++ app.js
@@ -48,7 +48,11 @@ app.get('/campgrounds/:id/edit', async (req, res) => {
 
 app.patch('/campgrounds/:id', async (req, res) => {
   const id = req.params.id;
-  const campground = await Campground.findByIdAndUpdate(id, req.body);
+  console.log(req.body.campground);
+  const campground = await Campground.findByIdAndUpdate(
+    id,
+    req.body.campground,
+  );
   res.redirect(`/campgrounds/${id}`);
 });
 
